f33735c28998c1a874e0d422b61b8c0106bca001
duxinyi <G1656790@www.com>
2023-06-15 16:50:56 +0800

    pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import "@/styles/common.scss";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+// 引入数据持久化插件
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
 import App from "./App.vue";
 import router from "./router";
 // 引入懒加载插件
@@ -11,8 +14,11 @@ import { lazyPlugin } from "@/directives";
 // 引用全局组件插件
 import { componentPlugin } from "@/components/index.js";
 const app = createApp(App);
-app.use(createPinia());
+const pinia = createPinia();
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate);
 app.use(router);
+app.use(pinia);
 
 app.mount("#app");
 //注册懒加载插件
