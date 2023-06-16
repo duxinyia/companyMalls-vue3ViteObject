5b6d1a93fd1add6d0b3c582e30385006bcfae1a8
duxinyi <2580191658@qq.com>
2023-06-16 21:27:34 +0800

    token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from "axios";
 // 如果使用 unplugin-element-plus 并且只使用组件 API，你需要手动导入样式。
 import "element-plus/theme-chalk/el-message.css";
 import { ElMessage } from "element-plus";
+import { useUserStore } from "@/stores/user";
 const httpInstance = axios.create({
   baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
   timeout: 5000,
@@ -10,6 +11,13 @@ const httpInstance = axios.create({
 // axios请求拦截器
 httpInstance.interceptors.request.use(
   (config) => {
+    // 从pinia获取token数据
+    let userStore = useUserStore();
+    let token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    // 按照后端的要求拼接token数据
     return config;
   },
   (e) => Promise.reject(e)
