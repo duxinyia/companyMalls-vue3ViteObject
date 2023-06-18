fd4a28e89a37f6438968986dce559c1efa2adf2e
duxinyi <2580191658@qq.com>
2023-06-18 17:41:54 +0800

    计算功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,6 +1,6 @@
 // 封装购物车模块
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 export const useCartStore = defineStore(
   "cart",
   () => {
@@ -23,7 +23,16 @@ export const useCartStore = defineStore(
       let idx = cartList.value.findIndex((item) => skuId === item.skuId);
       cartList.value.splice(idx, 1);
     };
-    return { cartList, addCart, delCart };
+    // 计算属性
+    // 1.总的数量 所有项的count之和
+    const allCount = computed(() => {
+      return cartList.value.reduce((a, c) => a + c.count, 0);
+    });
+    // 2.总价 所有项的count*price之和
+    const allPrice = computed(() => {
+      return cartList.value.reduce((a, c) => a + c.count * c.price, 0);
+    });
+    return { cartList, allCount, allPrice, addCart, delCart };
   },
   {
     persist: true,
